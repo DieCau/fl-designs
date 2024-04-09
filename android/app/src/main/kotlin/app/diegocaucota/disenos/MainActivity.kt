1426fd6a6eead4fcde2308b2e0650d553fc166d6
Diego Caucota <diegocaucota@gmail.com>
2024-04-09 09:28:51 -0300

    Custom Headers created

---
+++ android/app/src/main/kotlin/app/diegocaucota/disenos/MainActivity.kt
@@ -0,0 +1,5 @@
+package app.diegocaucota.disenos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/src/pages/headers_page.dart
@@ -0,0 +1,13 @@
+import 'package:disenos/src/widgets/headers_widget.dart';
+import 'package:flutter/material.dart';
+
+class HeadersPage extends StatelessWidget {
+  const HeadersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: HeaderWaveGradient()
+    );
+  }
+}
